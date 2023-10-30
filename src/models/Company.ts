9b5eec1acd789bdbfd7ac9142bb24295e500bcfc
Rik <rikw2412@gmail.com>
2023-10-30 11:02:49 +0100

    add models

---
+++ src/models/Company.ts
@@ -0,0 +1,12 @@
+export class Company{
+    declare id : number
+    declare name : string
+    declare info : string
+    declare profileImageId : number
+    declare bannerImageId : number
+    declare tags : string[]
+    declare branchId : number
+    declare createdAt : Date
+    declare ownerId : number 
+}
+
