c63503722fbca21f0a52a36e9ee88be7c4c5807e
Rik <66070072+Rikk010@users.noreply.github.com>
2023-11-03 13:18:53 +0100

    Add upload image (#17)
    add upload image

--- src/Api.ts
+++ src/Api.ts
@@ -1,5 +1,6 @@
 import { Challenge } from "./models/Challenge"
 import { ChallengeInput } from "./models/ChallengeInput"
+import { Image } from "./models/Image"
 import { User } from "./models/User"
 
 async function postRequest<T>(url: string, bodyObject: {}) {
@@ -14,6 +15,16 @@ async function postRequest<T>(url: string, bodyObject: {}) {
         body: JSON.stringify(bodyObject)
     })
     return await res.json() as T
+}
+async function uploadFile<T>(url: string, keyName : string, file: File) {
+    const formData = new FormData();
+    formData.append(keyName, file);
+    const response = await fetch(API.BASEURL + url, {
+        method: 'POST',
+        body: formData,
+    });
+    const data = await response.json();
+    return data as T
 }
 
 async function getRequest<T>(url: string) {
@@ -59,6 +70,9 @@ namespace API {
         return getRequest<Challenge[]>(urlstring);
     }
 
+    export async function uploadImage(img : File){
+        return uploadFile<Image>("image/upload", "image", img)
+    }
 
 }
 
