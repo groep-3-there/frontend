5bc73a3d8ada1d746766a2fa2db3c30bfdcf7c7b
Rik <66070072+Rikk010@users.noreply.github.com>
2023-11-03 13:19:50 +0100

    Feature ds5 afronden archiveren (#15)
    * feature-DS9-challenge-maken
    
    Added CreateChallengdeView
    
    * challenge is now based on backend
    
    * feature-DS9-challenge-maken
    
    Added createChallenge function
    
    * resolve conflict 1
    
    * add models
    
    * try add ci
    
    * add archive and afronden
    
    ---------
    
    Co-authored-by: EelcoJansma <71636744+EelcoJansma@users.noreply.github.com>
    Co-authored-by: Eelco <eelco.jansma1@student.nhlstenden.com>

--- src/Api.ts
+++ src/Api.ts
@@ -16,6 +16,20 @@ async function postRequest<T>(url: string, bodyObject: {}) {
     })
     return await res.json() as T
 }
+
+async function putRequest<T>(url: string, bodyObject: {}) {
+    const res = await fetch(API.BASEURL + url, {
+        method: "PUT",
+        headers: {
+            "Content-Type": "application/json",
+            "Access-Control-Allow-Origin": "http://localhost:3000"
+        },
+        mode: "cors",
+        // credentials: "include",
+        body: JSON.stringify(bodyObject)
+    })
+    return await res.json() as T
+
 async function uploadFile<T>(url: string, keyName : string, file: File) {
     const formData = new FormData();
     formData.append(keyName, file);
@@ -40,12 +54,15 @@ async function getRequest<T>(url: string) {
 namespace API {
 
     export const BASEURL = "http://localhost:8080/api/v1/"
+    export const BASEURL = "http://localhost:8080/api/v1/"
 
     export const headers = {
         "Content-Type": "application/json",
         "Access-Control-Allow-Origin": "http://localhost:3000"
     }
 
+    export function createChallenge(ch: {}){
+        return postRequest("challenge", ch)
     export function createChallenge(ch: {}){
         return postRequest("challenge", ch)
     }
@@ -59,6 +76,7 @@ namespace API {
 
     export async function getChallengeById(id : number){
         return getRequest<Challenge>(`challenge/${id}`)
+        return getRequest<Challenge>(`challenge/${id}`)
     }
 
     export async function getChallengesBySearch(query? : string, company?: string[], branche?: string[], sort?: string){
@@ -70,6 +88,11 @@ namespace API {
         return getRequest<Challenge[]>(urlstring);
     }
 
+
+    export async function updateChallenge(ch : Challenge){
+        return putRequest<Challenge>("challenge/update", ch)
+    }
+    
     export async function uploadImage(img : File){
         return uploadFile<Image>("image/upload", "image", img)
     }
