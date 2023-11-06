514fbd1edb1b4e6b6850dfb870711bded76015bc
Rik <rikw2412@gmail.com>
2023-11-06 13:46:06 +0100

    clear main

--- src/models/ChallengeInput.ts
+++
@@ -1,9 +0,0 @@
-export class ChallengeInput{
-    declare id: number
-    declare authorId: number
-    declare type : string
-    declare text : string
-    declare isChosenAnswer: boolean
-    declare createdAt : Date
-
-}
