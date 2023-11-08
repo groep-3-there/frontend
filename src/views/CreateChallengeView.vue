9eb2f92f8478a009a7a38bb6556126a78d89522d
_Luke <70942550+LukevandePol@users.noreply.github.com>
2023-11-08 09:00:17 +0100

    feature-ds29-Added tooltips to fileinputs + auto formatter (#28)
    Added tooltips to fileinputs + auto formatter

--- src/views/CreateChallengeView.vue
+++ src/views/CreateChallengeView.vue
@@ -58,12 +58,31 @@
 
           <v-row class="fit-buttons make-high">
             <v-col>
-              <v-select v-model="visibility" variant="outlined" label="Zichtbaarheid" :items="visibilityItems" :item-props="visibilityProperties" class="date" :rules="[(v) => !!v || 'Dit veld is verplicht!']"></v-select>
+              <v-select
+                v-model="visibility"
+                variant="outlined"
+                label="Zichtbaarheid"
+                :items="visibilityItems"
+                :item-props="visibilityProperties"
+                class="date"
+                :rules="[(v) => !!v || 'Dit veld is verplicht!']"
+              ></v-select>
             </v-col>
           </v-row>
 
           <v-row>
-            <v-col>
+            <v-col class="d-flex justify-space-around">
+              <v-tooltip v-model="showBanner" location="top">
+                <template v-slot:activator="{ props }">
+                  <v-btn icon v-bind="props" class="tooltip" color="primary">
+                    <v-icon color="secundary"> mdi-information-variant </v-icon>
+                  </v-btn>
+                </template>
+                <span>
+                  <p>Maximale groote banner: 10MB</p>
+                  <p>Aangeraden aspect ratio: 16:9</p>
+                </span>
+              </v-tooltip>
               <v-file-input
                 accept="image/png, image/jpeg, image/svg"
                 label="Upload een banner"
@@ -71,14 +90,30 @@
                 chips
                 show-size
                 v-model="banner"
-                :rules="[(v) => (v.length == 0 || (v.length == 1 && v[0].size < 10000000)) || 'De grootte van het bestand moet kleiner zijn dan 10MB!']"
+                :rules="[
+                  (v) =>
+                    v.length == 0 ||
+                    (v.length == 1 && v[0].size < 10000000) ||
+                    'De grootte van het bestand moet kleiner zijn dan 10MB!',
+                ]"
               >
               </v-file-input>
             </v-col>
           </v-row>
 
           <v-row>
-            <v-col>
+            <v-col class="d-flex justify-space-around">
+              <v-tooltip v-model="showImages" location="top">
+                <template v-slot:activator="{ props }">
+                  <v-btn icon v-bind="props" class="tooltip" color="primary">
+                    <v-icon color="secundary"> mdi-information-variant </v-icon>
+                  </v-btn>
+                </template>
+                <span>
+                  <p>Maximaal 8 afbeeldingen</p>
+                  <p>Maximale groote per afbeelding: 10MB</p>
+                </span>
+              </v-tooltip>
               <v-file-input
                 accept="image/png, image/jpeg, image/svg"
                 label="Upload afbeeldingen"
@@ -159,27 +194,57 @@ const title = ref("");
 const summary = ref("");
 const description = ref("");
 const contactInformation = ref("");
-const visibilityItems = [{title:'Publiek', subtitle:'Iedereen, ook zonder account', codeName: "PUBLIC"}, {title:'Intranet', subtitle:'Iedereen met een account', codeName: "INTRANET"}, {title:'Intern', subtitle:'Iedereen van uw bedrijf', codeName:"INTERNAL"}, {title:'Afdeling', subtitle: 'Iedereen van uw afdeling', codeName:"DEPARTMENT"}]
+const visibilityItems = [
+  {
+    title: "Publiek",
+    subtitle: "Iedereen, ook zonder account",
+    codeName: "PUBLIC",
+  },
+  {
+    title: "Intranet",
+    subtitle: "Iedereen met een account",
+    codeName: "INTRANET",
+  },
+  {
+    title: "Intern",
+    subtitle: "Iedereen van uw bedrijf",
+    codeName: "INTERNAL",
+  },
+  {
+    title: "Afdeling",
+    subtitle: "Iedereen van uw afdeling",
+    codeName: "DEPARTMENT",
+  },
+];
 const visibility = ref(null);
 const banner = ref([]);
 const images = ref([]);
 const tags = ref([]);
 const date = ref("");
-function visibilityProperties (item : any) {
+function visibilityProperties(item: any) {
   return {
     title: item.title,
     subtitle: item.subtitle,
-  }
+  };
 }
-function getVisibilityCodeName (title : string){
-  return visibilityItems.find((item) => item.title === title)?.codeName
+function getVisibilityCodeName(title: string) {
+  return visibilityItems.find((item) => item.title === title)?.codeName;
 }
 const createChallengeForm = ref(null) as any;
 
+/**
+ * show the tooltip for the banner
+ * default is false
+ */
+const showBanner = ref(false);
 
+/**
+ * show the tooltip for the images
+ * default is false
+ */
+const showImages = ref(false);
 
 async function createChallenge() {
-
   const { valid } = await createChallengeForm.value.validate();
   if (!valid || visibility.value == null) {
     alert("Alle vereiste velden zijn nog niet ingevuld!");
@@ -192,19 +257,19 @@ async function createChallenge() {
       tagString += tag + ",";
     });
   }
-  
+
   //upload banner
-  let uploadedBannerId = null
-  if(banner.value?.length){
-    const response = await Api.uploadImage(banner.value[0])
-    uploadedBannerId = response.id
+  let uploadedBannerId = null;
+  if (banner.value?.length) {
+    const response = await Api.uploadImage(banner.value[0]);
+    uploadedBannerId = response.id;
   }
-  
+
   //Upload attachments and get their ids
-  const attachmentImages : number[] = []
-  for(const toUpload of images.value){
-    const img = await Api.uploadImage(toUpload)
-    attachmentImages.push(img.id)
+  const attachmentImages: number[] = [];
+  for (const toUpload of images.value) {
+    const img = await Api.uploadImage(toUpload);
+    attachmentImages.push(img.id);
   }
   const challenge = {
     title: title.value,
@@ -218,9 +283,8 @@ async function createChallenge() {
     tags: tagString,
     visibility: getVisibilityCodeName(visibility.value),
   };
-  console.log("Creating challenge", challenge)
+  console.log("Creating challenge", challenge);
   const created = await Api.createChallenge(challenge);
- 
 
   createdChallenge.value = created;
   console.log(createdChallenge.value);
@@ -229,7 +293,6 @@ async function createChallenge() {
 </script>
 
 <style scoped>
-
 .date {
   max-width: 11rem;
 }
@@ -240,5 +303,9 @@ async function createChallenge() {
 
 h1 {
   padding: 4rem 0 0 0;
+}
+
+.tooltip {
+  margin: 0 2rem 0 0;
 }
 </style>
