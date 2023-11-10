3cc01fd09439ee0975693fd48d54bd0312a61060
Rik <66070072+Rikk010@users.noreply.github.com>
2023-11-10 22:32:30 +0100

    change ci 24

--- src/router/index.ts
+++ src/router/index.ts
@@ -43,15 +43,15 @@ const routes = [
     ],
   },
 ]
-
 const router = createRouter({
-  
   history: createWebHistory(process.env.BASE_URL),
   routes,
 })
 
 
 router.beforeEach((to, from, next) => {
+  console.warn("process.env.BASE_URL", process.env.BASE_URL)
+  console.warn("process.env.BASE_NAME", process.env.BASE_NAME)
   document.title = to.name as string
   const sessionStore = useSessionStore()
   // sessionStore.forceUpdate()
