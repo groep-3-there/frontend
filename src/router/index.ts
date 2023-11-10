64da9f632e90c1800e1c06c613fd198c5304bb88
Rik <66070072+Rikk010@users.noreply.github.com>
2023-11-10 22:21:28 +0100

    change ci 19

--- src/router/index.ts
+++ src/router/index.ts
@@ -45,7 +45,7 @@ const routes = [
 ]
 
 const router = createRouter({
-  history: createWebHistory(process.env.BASE_URL),
+  history: createWebHistory(process.env.BASE_URL + 'frontend/'),
   routes,
 })
 
