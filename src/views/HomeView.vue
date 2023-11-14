74be8c23fc94acca0adc94be1e2e2554e21e05d2
Rik <66070072+Rikk010@users.noreply.github.com>
2023-11-14 14:34:47 +0100

    change home

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,7 +1,28 @@
 <template>
     <HelloWorld />
-</template>
-
-<script lang="ts" setup>
-import HelloWorld from "@/components/HelloWorld.vue";
-</script>
+  
+    <h2>Voor debug mogelijkheden kan hier ingelogd worden met verschillende accounts</h2>
+    <v-btn class="mx-2" @click="login(1)">Login als user Jan Bakker</v-btn>
+  
+    <v-btn @click="test()">test</v-btn>
+    <br>
+    {{ whoami }}
+  </template>
+  
+  <script lang="ts" setup>
+  import API from '@/Api';
+  import HelloWorld from '@/components/HelloWorld.vue'
+  import { useSessionStore } from '@/store/sessionStore';
+  import { ref } from 'vue';
+  const sessionStore =useSessionStore()
+  const whoami = ref({})
+  async function login(name: number){
+    const success = await sessionStore.logIn(`${name}@mm.com`, `mmuser${name}`) 
+    console.log("Login success: " , success)
+  }
+  async function test(){
+    const r = await API.whoami()
+    whoami.value = r
+  }
+  </script>
+  
