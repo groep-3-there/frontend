8d3d74a8ddbe18d8a69a968582c6f8114a811e7c
_Luke <70942550+LukevandePol@users.noreply.github.com>
2023-11-14 16:51:33 +0100

    stepper wip

---
+++ src/components/UserRegister.vue
@@ -0,0 +1,32 @@
+<template>
+    <v-dialog transition="dialog-bottom-transition" width="auto">
+        <template v-slot:activator="{ props }">
+            <v-btn color="primary" v-bind="props">registreer</v-btn>
+        </template>
+        <template v-slot:default="{ isActive }">
+            <v-card>
+                <v-stepper alt-labels :items="registratieStappen">
+                    <template v-slot="item.1">
+                        <v-card title="Step One" flat>...</v-card>
+                    </template>
+                    <template v-slot="item.2">
+                        <v-card title="Step twee" flat>...</v-card>
+                    </template>
+                    <template v-slot="item.3">
+                        <v-card title="Step drie" flat>...</v-card>
+                    </template>
+                </v-stepper>
+                <v-card-actions class="justify-end">
+                    <v-btn variant="text" @click="isActive.value = false"
+                        >Close</v-btn
+                    >
+                </v-card-actions>
+            </v-card>
+        </template>
+    </v-dialog>
+</template>
+
+<script setup lang="ts">
+const registratieStappen = ["Account gegevens", "Naam", "Bedrijfscode"];
+console.log(registratieStappen[1])
+</script>
--- src/views/UserRegistrationView.vue
+++ src/views/UserRegistrationView.vue
@@ -1,23 +1,11 @@
 <template>
-    <v-stepper>
-        <v-stepper-header>
-            <v-stepper-item
-                title="Select campaign settings"
-                value="1"
-            ></v-stepper-item>
-
-            <v-divider></v-divider>
-
-            <v-stepper-item
-                title="Create an ad group"
-                value="2"
-            ></v-stepper-item>
-
-            <v-divider></v-divider>
-
-            <v-stepper-item title="Create an ad" value="3"> </v-stepper-item>
-        </v-stepper-header>
-    </v-stepper>
+    <h1>Hieronder hoort een stepper</h1>
+    <!-- <UserRegister></UserRegister> -->
+    <Stepper>
+    </Stepper>
 </template>
 
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import UserRegister from '@/components/UserRegister.vue'
+import Stepper from '@/components/stepper.vue'
+</script>
