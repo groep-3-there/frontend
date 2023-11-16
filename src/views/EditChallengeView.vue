6404835e5166c74657aab79cc6f08d38defa0f3a
Rik <66070072+Rikk010@users.noreply.github.com>
2023-11-16 10:35:20 +0100

    fix

--- src/views/EditChallengeView.vue
+++ src/views/EditChallengeView.vue
@@ -96,7 +96,8 @@
 
                     <v-row
                         v-if="
-                            originalChallenge?.bannerImageId !== null &&
+                            originalChallenge?.bannerImageId &&
+                            
                             banner.length == 0
                         "
                     >
