9b2efdbadd25a24ab652d337cbad1fac681d319f
_Luke <70942550+LukevandePol@users.noreply.github.com>
2023-11-21 14:11:47 +0100

    home pagina

---
+++ src/components/Homepage.vue
@@ -0,0 +1,23 @@
+<template>
+    <v-container>
+        <v-row>
+            <v-col class="d-flex justify-center align-center">
+                <h1>Hallo {{ sessionStore.loggedInUser?.name }}</h1>
+            </v-col>
+        </v-row>
+        <NewestChallenges
+        :company="sessionStore.loggedInUser?.department?.parentCompany"
+        ></NewestChallenges>
+    </v-container>
+</template>
+
+<script setup lang="ts">
+import { useSessionStore } from "@/store/sessionStore";
+import NewestChallenges from "./NewestChallenges.vue";
+
+const sessionStore = useSessionStore();
+</script>
+
+<style scoped>
+
+</style>
---
+++ src/components/NewestChallenges.vue
@@ -0,0 +1,47 @@
+<template>
+    <v-row>
+        <v-col class="d-flex align-center">
+            <h2>Nieuwste challenges van {{ props.company?.name }}</h2>
+        </v-col>
+    </v-row>
+    <v-row>
+        <v-col>
+            <div class="d-flex flex-row">
+                <ChallengeCard
+                    v-for="challenge in challenges"
+                    :key="challenge.id"
+                    :challenge="challenge"
+                    :company="company!"
+                />
+            </div>
+        </v-col>
+    </v-row>
+</template>
+
+<script lang="ts" setup>
+import API from "@/Api";
+import { Challenge } from "@/models/Challenge";
+import { Company } from "@/models/Company";
+import { on } from "events";
+import { ref, onMounted } from "vue";
+import ChallengeCard from "./ChallengeCard.vue";
+
+const props = defineProps({
+    company: Company,
+});
+
+const challenges = ref<Challenge[]>([]);
+
+onMounted(async () => {
+    // const NewestChallenges = await API.getAllChallengesForCompany(
+    //     props.company!.id,
+    // );
+    // challenges.value = NewestChallenges;
+
+    await API.getAllChallengesForCompany(props.company!.id).then(
+        (response) => (challenges.value = response),
+    );
+});
+</script>
+
+<style scoped></style>
