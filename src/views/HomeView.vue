4932cd9093bc8bd429ec6cf562718b2a198b9fdf
Rik <66070072+Rikk010@users.noreply.github.com>
2023-11-24 20:46:18 +0100

    Update HomeView.vue

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -9,8 +9,6 @@
 
 <script lang="ts" setup>
 import { useSessionStore } from "@/store/sessionStore";
-import { ref } from "vue";
-import InviteListener from "@/components/InviteListener.vue";
 const sessionStore = useSessionStore();
 import LandingPage from "@/components/LandingPage.vue";
 import Homepage from "@/components/Homepage.vue";
