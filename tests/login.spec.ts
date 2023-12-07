7abd6d8f27daac75a4a12cd3081abfa3251ddf94
TjerkVenema <Tjerk.Venema@student.nhlstenden.com>
2023-12-07 11:00:10 +0100

    Update login.spec.ts

--- tests/login.spec.ts
+++ tests/login.spec.ts
@@ -1,32 +1,28 @@
 import { test, expect } from '@playwright/test';
 import * as jsonFiles from './jsonFiles.ts';
+import { assert } from 'console';
 
 test.beforeEach(async ({ page }) => {
   await page.goto('http://localhost:3000/');
 });
 
 test('get a challenge', async ({ page }) => {
-  try {
-      await page.route('*/**/api/v1/challenge/search?', async (route) => {
-          await route.fulfill({
-              json: jsonFiles.mockSearchResult
-          });
-      });
+    await page.route('*/**/api/v1/challenge/search?', async (route) => {
+        await route.fulfill({
+            json: jsonFiles.mockSearchResult
+        });
+    });
 
-      await page.route('*/**/api/v1/reaction/challenge/1', async (route) => {
-          await route.fulfill({
-              json: jsonFiles.mockChallenge
-          });
-      });
+    await page.route('*/**/api/v1/reaction/challenge/1', async (route) => {
+        await route.fulfill({
+            json: jsonFiles.mockChallenge
+        });
+    });
 
-      await page.goto('http://localhost:3000/');
-      await page.locator('div').filter({ hasText: /^Zoeken$/ }).first().click({delay: 1000});
-      await page.locator('div').filter({ hasText: /^Zoeken$/ }).first().click({delay: 1000});
-      await page.getByText('LogiTech Solutions BV zoekt').click({delay: 1000});
-      await page.getByText('LogiTech Solutions BV zoekt').click({delay: 1000});
-      await expect(page.getByRole('heading', { name: 'Optimalisatie van Logistieke' })).toBeVisible();
-
-  } catch (error) {
-      throw new Error(`Test failed: ${error.message}`);
-  }
+    await page.goto('http://localhost:3000/');
+    await page.locator('div').filter({ hasText: /^Zoeken$/ }).first().click({delay: 1000});
+    await page.locator('div').filter({ hasText: /^Zoeken$/ }).first().click({delay: 1000});
+    await page.getByText('LogiTech Solutions BV zoekt').click({delay: 1000});
+    await page.getByText('LogiTech Solutions BV zoekt').click({delay: 1000});
+    await expect(page.getByRole('heading', { name: 'Optimalisatie van Logistieke' })).toBeVisible();
 });
