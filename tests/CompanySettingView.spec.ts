9777d3fca2617e7727310f2b438bb2d31f0f1615
_Luke <70942550+LukevandePol@users.noreply.github.com>
2023-12-09 00:47:15 +0100

    changed test

--- tests/CompanySettingView.spec.ts
+++ tests/CompanySettingView.spec.ts
@@ -34,12 +34,12 @@ test.beforeEach(async ({ page }) => {
 });
 
 test('Change roles', async ({ page }) => {
-  await page.getByRole('button', { name: 'Afdeling beheerder Open' }).click();
   await expect(page.getByRole('heading', { name: 'Jelle Jacobs' })).toBeVisible();
   await expect(page.getByRole('heading', { name: 'Eline de Groot' })).toBeVisible();
+  await page.getByRole('button', { name: 'Afdeling beheerder Open' }).click();
   await page.locator('div').filter({ hasText: /^Medewerker$/ }).nth(2).click();
   await page.getByRole('button', { name: 'Medewerker Open' }).nth(1).click();
-  await page.locator('.v-overlay__content > .v-list > div:nth-child(2)').click();
+  await page.locator('div').filter({ hasText: /^Medewerker$/ }).nth(2).click();
   await page.getByRole('button', { name: 'Update Rollen' }).click();
   await expect(page.getByText('Een afdeling heeft altijd een')).toBeVisible();
   await page.getByRole('button', { name: 'Medewerker Open' }).first().click();
