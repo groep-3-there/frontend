0299407ca2333aea5e511ef5ed8e2eaccdfc6af9
Rik <rikw2412@gmail.com>
2023-12-11 12:11:09 +0100

    change timeout

--- playwright.config.ts
+++ playwright.config.ts
@@ -70,6 +70,7 @@ export default defineConfig({
     command: 'npm run dev',
     url: 'http://localhost:3000/',
     reuseExistingServer: !process.env.CI,
+    timeout: 120000,
     stdout: 'ignore',
     stderr: 'pipe',
   },
