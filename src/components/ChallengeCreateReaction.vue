d250f5fabc134dfbb5797fe76f0469b4919ea2f9
Rik <rikw2412@gmail.com>
2024-01-09 09:08:31 +0100

    merge 1

--- src/components/ChallengeCreateReaction.vue
+++ src/components/ChallengeCreateReaction.vue
@@ -3,25 +3,19 @@
         @model-value-changed="(c: string) => (newReactionContent = c)"
         :placeholder="`Uw reactie`"
     ></RichEditor>
-    <v-spacer class="my-4"></v-spacer>
-    <v-row class="d-flex justify-end align-center">
-        <v-col cols="8" md="4">
-            <div class="reaction-options">
-                <div class="reaction-type-selector d-flex justify-center align-center">
-                    <v-select
-                        variant="outlined"
-                        v-model="newReactionType"
-                        label="Type Reactie"
-                        :items="['Idee', 'Vraag', 'Feedback']"
-                        >Type</v-select
-                    >
-                </div>
-            </div>
-        </v-col>
+    <div class="reaction-options">
+        <div class="reaction-type-selector d-flex justify-center align-center">
+            <v-select
+                variant="outlined"
+                v-model="newReactionType"
+                label="Type Reactie"
+                :items="['Idee', 'Vraag', 'Feedback']"
+                >Type</v-select
+            >
+        </div>
         <div class="d-flex justify-center align-center">
             <v-btn @click="postReaction" color="primary">Plaatsen</v-btn>
         </div>
-    </v-row>
 </template>
 
 <script setup lang="ts">
